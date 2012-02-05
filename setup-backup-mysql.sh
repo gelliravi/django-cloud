9af804461689120881625a5da8393c4ff3d7e31a
Deepak Garg <deepakgarg.iitg@gmail.com>
2012-02-05 15:39:18 +0530

    Basic mysql file added

---
+++ setup-backup-mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set +ex
+
+usage() {
+cat <<EOF
+Usage: $SCRIPT_NAME <MYSQL-ROOT-PASSWORD> 
+Example: $SCRIPT_NAME  mysqlsecret
+EOF
+}
+
+if [ $# -ne 1 ]; then
+   usage
+   exit 1
+fi
+
+#export DEBIAN_FRONTEND=noninteractive
+PASSWORD=$1
+
+#TODO -> make apt-get non-interactive and no ouptut
+install_updates() {
+sudo apt-get update && sudo apt-get upgrade -y
+}
+
+install_baseline() {
+sudo apt-get install -y git-core build-essential curl
+}
+
+install_mysql() {
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+}
+
+install_updates && install_baseline && install_mysql
+out=$?
+if [ $out -ne 0 ]; then
+    die "$SCRIPT_NAME Couldnt complete Mysql Install" out
+    usage
+    exit out 
+fi
+
+sleep 3 #waiting for mysql to restart
+mysqladmin -uroot password $PASSWORD
+sudo service mysql restart
+
+set -ex
+
+
+
